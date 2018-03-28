d9d58f1e37903c9808f460684345ced84eae5ef3
Bao Nguyen The Vinh <bao.nguyen150028@gmail.com>
2018-03-28 23:20:54 +0900

    Added form and validate function

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,13 +1,63 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
-export default class PostsNew extends Component { 
-   
+class PostsNew extends Component { 
+   renderField(field) {
+    return (
+        <div className="form-group">
+            <label> {field.label}  </label>
+            <input 
+                className="form-control"
+                type="text"
+                {...field.input}
+            />
+        </div>
+    )
+   }
+   renderTagsField
     render() {
         return (
-            <div>
-               Post new index
-            </div>
+            <form>
+                <Field 
+                    label="Title"
+                    name="title" 
+                    component={this.renderField}
+                />
+                <Field 
+                    label="Categories"
+                    name="categories" 
+                    component={this.renderField}
+                />
+                <Field 
+                    label="Post Content"
+                    name="content" 
+                    component={this.renderField}
+                />
+            </form>
         )
     }
 }
 
+function validate(values) {
+    const errors = {
+
+    };  
+    
+    if (!values.title) {
+        errors.title = "Enter a title!";
+    }
+    if (!values.categories) {
+        errors.categories = "Enter some categories!";
+    }
+    if (!values.content) {
+        errors.content = "Enter some content!";
+    }
+
+    return erros;
+}
+
+export default reduxForm({
+    validate,
+    form: 'PostsNewForm'
+
+})(PostsNew);
